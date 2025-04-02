a57822b72da5bd0862b16ebc1fd88173d6e4849d
katniny <okuwu1680@gmail.com>
2025-04-02 11:05:40 -0600

    Bump version

--- assets/js/versioning.js
+++ assets/js/versioning.js
@@ -1,5 +1,5 @@
 let transsocialVersion = "v2025.4.2";
-let transsocialUpdate = "v2025042-3";
+let transsocialUpdate = "v2025042-4";
 let transsocialReleaseVersion = "pre-alpha";
 let hasUpdateNotes = true;
 
