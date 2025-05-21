8ff648314d43d3597132af3ffc47da70d27abc28
katniny <okuwu1680@gmail.com>
2025-05-21 17:38:20 -0600

    Update version & update logs

--- assets/js/versioning.js
+++ assets/js/versioning.js
@@ -5,5 +5,5 @@ let hasUpdateNotes = true;
 
 const notices = document.getElementsByClassName("version-notice");
 for (let notice of notices) {
-   notice.innerHTML = `TransSocial is currently in the pre-alpha stage (version ${transsocialVersion}). A lot of features are missing or are in development and will be added with updates. <a href="/indev">Learn more</a>.`;
+   notice.innerHTML = `TransSocial is currently in the ${transsocialReleaseVersion} stage (version ${transsocialVersion}). A lot of features are missing or are in development and will be added with updates. <a href="/indev">Learn more</a>.`;
 }
