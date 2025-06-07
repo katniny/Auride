df301207caf074187e2948e850e76e2701ce63ea
piar <142241267+piarsquared@users.noreply.github.com>
2025-06-07 16:15:23 -0400

    All updated files
    Fixed note bug with the notes that would visually be doubled

---
+++ versioning.js
@@ -0,0 +1,9 @@
+let transsocialVersion = "v2025.6.7";
+let transsocialUpdate = "v2025067-2";
+let transsocialReleaseVersion = "pre-alpha";
+let hasUpdateNotes = false;
+
+const notices = document.getElementsByClassName("version-notice");
+for (let notice of notices) {
+   notice.innerHTML = `TransSocial is currently in the ${transsocialReleaseVersion} stage (version ${transsocialVersion}). A lot of features are missing or are in development and will be added with updates. <a href="/indev">Learn more</a>.`;
+}
