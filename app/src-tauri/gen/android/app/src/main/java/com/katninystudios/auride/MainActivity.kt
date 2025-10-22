8d225f2a7fde99c26fa9d7f7d9130dbe28e160ef
katniny <110915403+katniny@users.noreply.github.com>
2025-10-22 00:18:30 -0600

    Add Auride Android app (#94)
    * Add Android build support
    
    * Fix targets
    
    * Separate Android build from desktop
    
    * Update gradle.properties
    
    * Enable AndroidX
    
    * Increase RAM
    
    * Push Android APK to latest release
    
    * Use bash instead
    
    maybe doing officially supported ways is better than... actions
    
    * Fix Windows and Android builds
    
    * oops
    
    * Rename Android app before release
    
    * Use literal name
    
    * Maybe too deep? & add debug ls
    
    * Make instruction for Android accurate
    
    * Remove test branch

---
+++ app/src-tauri/gen/android/app/src/main/java/com/katninystudios/auride/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.katninystudios.auride
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
