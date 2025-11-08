f3b2118e8eb26715b0a5930cc712201a5683d94c
katniny <110915403+katniny@users.noreply.github.com>
2025-11-07 20:41:43 -0700

    Comment out update code

--- src/public/assets/js/ui/updateAvailablePopup.js
+++ src/public/assets/js/ui/updateAvailablePopup.js
@@ -1,29 +1,32 @@
-const updateModal = `
-    <h2><i class="fa-solid fa-cloud-arrow-down"></i> App Update Available</h2>
-    <p>Please note that there are no auto-updates yet, Auride will not update for you! Auto-updates are a planned feature.</p>
-    <p>If you don't update, Auride will continue to load the website properly, but you'll miss out on new app features, security updates, and more.</p>
+// TODO: figure out why this is not working
+// and potentionally blocking other app functionality 
 
-    <br />
+// const updateModal = `
+//     <h2><i class="fa-solid fa-cloud-arrow-down"></i> App Update Available</h2>
+//     <p>Please note that there are no auto-updates yet, Auride will not update for you! Auto-updates are a planned feature.</p>
+//     <p>If you don't update, Auride will continue to load the website properly, but you'll miss out on new app features, security updates, and more.</p>
 
-    <a href="https://github.com/katniny/Auride/releases" target="_blank"><button>Download Update</button></a> <button onclick="closeUpdatePopup()">Dismiss</button>
-`;
+//     <br />
 
-function showNewUpdateAvailablePopup() {
-    // append and put the content inside the modal
-    const updateAvailablePopup = document.createElement("dialog");
-    updateAvailablePopup.setAttribute("id", "updateAvailablePopup");
-    updateAvailablePopup.innerHTML = updateModal;
-    document.body.appendChild(updateAvailablePopup);
+//     <a href="https://github.com/katniny/Auride/releases" target="_blank"><button>Download Update</button></a> <button onclick="closeUpdatePopup()">Dismiss</button>
+// `;
 
-    // then, finally show modal
-    updateAvailablePopup.showModal();
-}
+// function showNewUpdateAvailablePopup() {
+//     // append and put the content inside the modal
+//     const updateAvailablePopup = document.createElement("dialog");
+//     updateAvailablePopup.setAttribute("id", "updateAvailablePopup");
+//     updateAvailablePopup.innerHTML = updateModal;
+//     document.body.appendChild(updateAvailablePopup);
 
-function closeUpdatePopup() {
-    const notePopup = document.getElementById("updateAvailablePopup");
+//     // then, finally show modal
+//     updateAvailablePopup.showModal();
+// }
 
-    notePopup.close();
-    setTimeout(() => {
-        notePopup.remove();
-    }, 500);
-}
+// function closeUpdatePopup() {
+//     const notePopup = document.getElementById("updateAvailablePopup");
+
+//     notePopup.close();
+//     setTimeout(() => {
+//         notePopup.remove();
+//     }, 500);
+// }
