2e9de6a0940f36b8e5fbefb6a7d31b9d74a2b624
katniny <okuwu1680@gmail.com>
2025-12-17 20:26:08 -0700

    :(

--- src/public/assets/js/ui/underConstruction.js
+++
@@ -1,3 +0,0 @@
-setTimeout(() => {
-    window.location.replace("/maintenance");
-}, 500);
--- src/types/tus.d.ts
+++
@@ -1,5 +0,0 @@
-import { Upload } from "tus-js-client";
-
-declare global {
-  var tus: { Upload: typeof Upload };
-}
