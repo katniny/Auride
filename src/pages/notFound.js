4e6550cf8c1c58b30a195263cee74b96cce08c1f
katniny <okuwu1680@gmail.com>
2026-02-02 00:35:42 -0700

    Add styling to 404 page

--- src/pages/notFound.js
+++ src/pages/notFound.js
@@ -3,21 +3,23 @@ export default function loadNotFound() {
     const el = document.createElement("div");
     const pathname = window.location.pathname;
     el.innerHTML = `
-        <img class="pageFailAurora" src="/assets/mascot/concerned.png" draggable="false" />
-        <h2>We were unable to find that page :(</h2>
-        <p class="description">
-            We were unable to find ${pathname}.
-            The page doesn't exist, got moved, or got lost to time.
-        </p>
+        <div class="pageNotFound">
+            <img class="pageFailAurora" src="/assets/mascot/concerned.png" draggable="false" />
+            <h2>We were unable to find that page :(</h2>
+            <p class="description">
+                We were unable to find ${pathname}.
+                The page doesn't exist, got moved, or got lost to time.
+            </p>
 
-        <br />
+            <br />
 
-        <a href="/u/${pathname}">Were you looking for a profile?</a>
+            <a href="/u/${pathname}">Were you looking for a profile?</a>
 
-        <br />
+            <br />
 
-        <p>Go to the home page? We know that page exists?</p>
-        <a href="/home"><button>Go Home</button></a>
+            <p>Go to the home page? We know that page exists!</p>
+            <a href="/home"><button>Go Home</button></a>
+        </div>
     `;
     return el;
 }
