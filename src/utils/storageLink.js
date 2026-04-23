162217ffcf6d1483054a1cf8db500b84241ec81e
katniny <okuwu1680@gmail.com>
2026-04-22 22:17:50 -0600

    Add comment

--- src/utils/storageLink.js
+++ src/utils/storageLink.js
@@ -1,10 +1,13 @@
 import { getMediaLink } from "../methods/dev/getMediaLink";
 
 export async function storageLink(path) {
+    // if in build environment, we have some tricks (due to weird docker issues)
+    // which is incompatible with prod.
     if (import.meta.env.VITE_BUILD_ENV) {
         // get id and return
         const id = await getMediaLink(path);
         return `${import.meta.env.VITE_STORAGE_URL}/files/${id}`;
     }
+    // if in prod, just return the path directly
     return `${import.meta.env.VITE_STORAGE_URL}/${path}`;
 }
