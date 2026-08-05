c4fdfe12babe5a54dac276b17ff9cefa7370a18f
Katty Paris <okuwu1680@gmail.com>
2026-08-04 21:56:25 -0600

    chore: update version

--- src/ui/versioning.js
+++ src/ui/versioning.js
@@ -1,7 +1,7 @@
 // formatted: vYEAR.MONTH.DAY
-const updateTime = "v2026.6.TBD";
+const updateTime = "v2026.8.25";
 // formatted: vYEARMONTHDAY
-const updateTimeSimple = "v20266TBD"
+const updateTimeSimple = "v2026825"
 // the number of updates today (increment by one, unless there was no commits today!)
 const numOfUpdatesToday = "1";
 // the current auride version. contributors should leave this alone
