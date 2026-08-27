9ec61e23443506fd6e1598a88c78531249fbb869
Katty Paris <okuwu1680@gmail.com>
2026-08-27 02:55:38 -0600

    fix: Fix prod trying to talk to dev environment

--- web/utils/storageLink.js
+++ web/utils/storageLink.js
@@ -3,7 +3,7 @@ import { getMediaLink } from "../methods/dev/getMediaLink";
 export async function storageLink(path) {
     // if in build environment, we have some tricks (due to weird docker issues)
     // which is incompatible with prod.
-    if (import.meta.env.VITE_BUILD_ENV) {
+    if (import.meta.env.VITE_BUILD_ENV === "DEV") {
         // get id and return
         const id = await getMediaLink(path);
         return `${import.meta.env.VITE_STORAGE_URL}/files/${id}`;
